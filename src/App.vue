ab9e1b887f8c67b32e618d2c5a7ca7a121e83fad
e274426380 <867690443@qq.com>
2021-06-28 19:18:48 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" behavior="desktop">
+      <!-- drawer content -->
+      <q-toolbar class="bg-primary text-white">
+      </q-toolbar>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" behavior="desktop">
+      <!-- drawer content -->
+      <q-toolbar class="bg-primary text-white">
+      </q-toolbar>
+    </q-drawer>
+
+    <q-page-container>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        left: false,
+        right: false
+      }
+    }
+  }
+</script>
